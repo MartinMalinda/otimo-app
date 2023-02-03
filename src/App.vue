9020941a2b61e50d04df4cb6a511fa1101a050aa
martinmalinda <malindacz@gmail.com>
2023-02-03 20:08:29 +0100

    add more vids

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,8 @@ import { ref } from 'vue';
 import { useSessionStore } from '/~/data/stores/session';
 import { useJourneyStore } from '/~/data/stores/journey';
 import Login from './components/Login.vue';
-import Nav from '/~/components/layout/Nav.vue';
 import { supabase } from '/~/data/supabase';
-import Container from '/~/components/layout/Container.vue';
+import Onboarding from '/~/components/Onboarding.vue';
 
 const sessionStore = useSessionStore();
 const journeyStore = useJourneyStore();
@@ -23,19 +22,7 @@ supabase.auth.getSession().then(({ data }) => {
   <template v-if="loggedIn === undefined" />
   <template v-else-if="loggedIn">
     <!-- <Nav class="nav" /> -->
-    <Container>
-      <div class="onboarding">
-        <h1>Welcome, traveller</h1>
-        <p>Otimo is a place designed to help you seek new horizons. To help you on your journey to become healthier,
-          happier, wealthier and in harmony with the planet and the global community.</p>
-        <h2>Let us know more about you</h2>
-        <p>First, let as usk a few questions so that we can help you better. Share only as much you'd like.</p>
-        <div class="buttons">
-          <button>Fill the survey</button>
-          <button class="secondary">Skip this step</button>
-        </div>
-      </div>
-    </Container>
+    <Onboarding />
     <div v-for="journey in journies">
       {{ journey }}
     </div>
@@ -46,23 +33,5 @@ supabase.auth.getSession().then(({ data }) => {
 <style scoped>
 .nav {
   margin-top: 16px;
-}
-
-.onboarding {
-  border: 1px solid black;
-  border-radius: 0px;
-  padding: 16px;
-}
-
-.buttons {
-  display: flex;
-  justify-content: space-between
-}
-
-button.secondary {
-  background: white;
-  color: darkgray;
-  border: 0;
-  font-weight: 400;
 }
 </style>
---
+++ src/components/Onboarding.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="onboarding">
+    <section>
+      <h1>Welcome, traveller</h1>
+      <h2>Otimo is designed to help you seek new perspectives.</h2>
+    </section>
+    <section>
+      <h2>It helps you take harmonious action...</h2>
+    </section>
+    <section>
+      <h3>To save money</h3>
+      <h3>to boost your health</h3>
+      <h3>to find inner peace...</h3>
+    </section>
+    <section>
+      <h3>and to be in symbiosis with others and the planet.</h3>
+    </section>
+    <h2>Let us know more about you</h2>
+    <p>First, let as usk a few questions so that we can help you better. Share only as much you'd like.</p>
+    <div class="buttons">
+      <button>Fill the survey</button>
+      <button class="secondary">Skip this step</button>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+h1 {
+  font-size: 80px;
+}
+
+.onboarding {
+  border-radius: 0px;
+  padding: 16px;
+}
+
+.buttons {
+  display: flex;
+  justify-content: space-between
+}
+
+section {
+  height: 60vh;
+  border: 1px solid black;
+  padding: 36px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  // align-items: center;
+}
+
+
+button.secondary {
+  background: white;
+  color: darkgray;
+  border: 0;
+  font-weight: 400;
+}
+</style>
